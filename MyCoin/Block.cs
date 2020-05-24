dde241735b51eee8187cbaef7e2437516d4ff4b4
buitrihieu98 <51129013+buitrihieu98@users.noreply.github.com>
2020-05-24 23:55:15 +0700

    Initial commit

---
+++ MyCoin/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoin
+{
+    class Block
+    {
+        public String hash;
+        public String previousHash;
+        private String data; 
+        private long timeStamp; 
+        private int nonce = 0;
+
+        //Block Constructor.
+        public Block(String data, String previousHash)
+        {
+            this.data = data;
+            this.previousHash = previousHash;
+            //this.timeStamp = DatetimeHandle.GetTime();
+            this.hash = CalculateHash();
+        }
+
+        private string CalculateHash()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
